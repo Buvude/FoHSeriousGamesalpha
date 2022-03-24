1f2d2d9a4e6d6895ebb6d856e3b46bb8673007f9
Buvude <90856761+Buvude@users.noreply.github.com>
2022-03-24 15:07:00 -0400

    fixxed

--- Assets/Scripts/GrammerHWScript.cs
+++ Assets/Scripts/GrammerHWScript.cs
@@ -7,21 +7,24 @@ public class GrammerHWScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public Text ProofReaderSentancePlaceHolder,proofReaderSentanceTrue;
+    public InputField iField;
     public string q1Correct = "Every time I walk along the beach, my dog runs up to me.";
     public string[] q1Incorrect = {"Ever time I walk along the beach, my dog runs up to me.", "Every time I walk along the birch, my dog runs up to me.","Every time I walk along the beach, my dog runs uo to me."};
     public int randomIndex;
     void Start()
     {
+        
         randomIndex = Random.Range(0, 2);
         ProofReaderSentancePlaceHolder.text = "Hello, this is a test";
         //ProofReaderSentance.text = q1Incorrect.GetValue(int.Parse(randomIndex.ToString())).ToString();
         Debug.Log("Text is saying: " + ProofReaderSentancePlaceHolder.text.ToString());
+        iField.text = ProofReaderSentancePlaceHolder.text;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     public void setStrings()
     {
