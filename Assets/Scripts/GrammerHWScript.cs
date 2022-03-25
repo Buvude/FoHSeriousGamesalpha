5aa81cd12e6a02444d734bf38e7ba47562862319
Buvude <90856761+Buvude@users.noreply.github.com>
2022-03-25 09:40:59 -0400

    I got the bare-bones of the proof reading HW!!

--- Assets/Scripts/GrammerHWScript.cs
+++ Assets/Scripts/GrammerHWScript.cs
@@ -6,19 +6,21 @@ using UnityEngine.UI;
 public class GrammerHWScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Text ProofReaderSentancePlaceHolder,proofReaderSentanceTrue;
+    public ScoreManager sM;
+    //public Text ProofReaderSentancePlaceHolder,proofReaderSentanceTrue;
     public InputField iField;
     public string q1Correct = "Every time I walk along the beach, my dog runs up to me.";
     public string[] q1Incorrect = {"Ever time I walk along the beach, my dog runs up to me.", "Every time I walk along the birch, my dog runs up to me.","Every time I walk along the beach, my dog runs uo to me."};
-    public int randomIndex;
+    public int randomIndex, proofReadingQuestionNumber;
+    public Button submitBtn, resetBtn, instructionsBtn;
     void Start()
     {
-        
+        proofReadingQuestionNumber = 1;
         randomIndex = Random.Range(0, 2);
-        ProofReaderSentancePlaceHolder.text = "Hello, this is a test";
-        //ProofReaderSentance.text = q1Incorrect.GetValue(int.Parse(randomIndex.ToString())).ToString();
-        Debug.Log("Text is saying: " + ProofReaderSentancePlaceHolder.text.ToString());
-        iField.text = ProofReaderSentancePlaceHolder.text;
+        //ProofReaderSentancePlaceHolder.text = "Hello, this is a test";
+        iField.text = q1Incorrect.GetValue(int.Parse(randomIndex.ToString())).ToString();
+        //Debug.Log("Text is saying: " + ProofReaderSentancePlaceHolder.text.ToString());
+        //iField.text = ProofReaderSentancePlaceHolder.text;
     }
 
     // Update is called once per frame
@@ -26,8 +28,39 @@ public class GrammerHWScript : MonoBehaviour
     {
        
     }
-    public void setStrings()
+   /* public void setStrings()
     {
         proofReaderSentanceTrue.text = "Hello, this is a test";
+    }*/
+
+    public void onSubmit()
+    {
+        if (proofReadingQuestionNumber == 1)
+        {
+            if (iField.text == q1Correct)
+            {
+                sM.incorrectAnswers.SetValue(false, sM.questionNumber);
+                sM.questionNumber++;
+                sM.correct++;
+                proofReadingQuestionNumber++;
+            }
+            else
+            {
+                sM.incorrectAnswers.SetValue(true, sM.questionNumber);
+                sM.questionNumber++;
+                sM.incorrect++;
+                proofReadingQuestionNumber++;
+            }
+        }
+       
+    }
+    public void onReset()
+    {
+        switch (proofReadingQuestionNumber)
+        {
+            case 1:
+                iField.text = q1Incorrect.GetValue(int.Parse(randomIndex.ToString())).ToString();
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int correct, incorrect,questionNumber;
+    public const int TOTAL=15;
+    public bool[] incorrectAnswers=new bool[15];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
