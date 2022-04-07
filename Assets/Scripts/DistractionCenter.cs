d2186bb3bcb6e7e80b753e99de08a755c2c86314
Buvude <90856761+Buvude@users.noreply.github.com>
2022-04-07 13:59:01 -0400

    good shit

--- Assets/Scripts/DistractionCenter.cs
+++ Assets/Scripts/DistractionCenter.cs
@@ -6,14 +6,16 @@ using TMPro;
 
 public class DistractionCenter : MonoBehaviour
 {
-    public AudioSource ding;
+    public AudioSource MentalDistraction, PhysichalDistraction, ComputerDistraction;
     public AudioClip SynthroidShort, SynthroidFull, dingSoud;
-    public TextMeshProUGUI posterText;
+    public TextMeshProUGUI posterText, notificationText;
+    public string[,] notifications;
+    public int notificationsQueued = 0, notificationsRead=0; 
     //public GameObject mentalChoices, phisichalChoices, computerChoices;
     // Start is called before the first frame update
     void Start()
     {
-        
+        notifications = new string[20,2];
     }
 
     // Update is called once per frame
@@ -29,18 +31,55 @@ public class DistractionCenter : MonoBehaviour
 
     public void MentalDistractionSpawn()
     {
-        ding.clip=SynthroidShort;
-        ding.loop=true;
-        ding.Play();
+        MentalDistraction.clip=SynthroidShort;
+        MentalDistraction.loop=true;
+        MentalDistraction.Play();
         posterText.text = "Mental distractions will have many different forms, but for this demo we have an earworm, or a song that is stuck in your head. According to research a good way to deal with this is to listen to the song from start to end. In the full game, the longer that the song is stuck in your head, the harder it is to focus, and the more anxiety you will get, and the louder it will become.";
         //mentalChoices.SetActive(true);
 
     }
     public void mentatlDistractitonSynthroidFullSong()
     {
-        ding.clip = SynthroidFull;
-        ding.loop = false;
-        ding.Play();
+        MentalDistraction.clip = SynthroidFull;
+        MentalDistraction.loop = false;
+        MentalDistraction.Play();
         //mentalChoices.SetActive(false);
     }
+    public void spawnPhysichalDistraction()
+    {
+        //TODO start conversation sound effect
+        posterText.text = "this will start a conversation that is distracting (probably a domestic dispute because it is distracting)";
+    }
+
+    public void spawnComputerDistraction()
+    {
+        posterText.text = "This will cause a notification sound effect and add a notification to the notification queue (which you can then check, and dismiss notifications or read them (some need to be read, or else you might get a question wrong))";
+        PhysichalDistraction.clip = dingSoud;
+        PhysichalDistraction.loop = false;
+        PhysichalDistraction.Play();
+        notificationsQueued++;
+        checkNotificationQueue();
+    }
+
+    public void checkNotificationQueue()
+    {
+        if (notificationsQueued > 0)
+        {
+            //TODO add 20 notifications and notification descriptions
+            notificationText.text = ("Notification number " + notificationsRead + 1);
+        }
+        else
+        {
+            notificationText.text = "No more notifications at this time!";
+        }
+    }
+
+    public void ignoreNotification()
+    {
+        notificationsQueued--;
+        notificationsRead++;
+        checkNotificationQueue();
+    }
+
+
 }
